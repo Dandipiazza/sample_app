1ef3409133b3769b1bcf0c4a719c039bf2ff5abc
Colossal Filmworks <ColossalFilmworks@Colossal-Filmworkss-MacBook-Pro.local>
2013-02-23 17:37:01 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '40953c6f68122693b3504dfa1555d99f1b4692b2cc1620250a2914a4b085a51634b7943c606e0d251fc5df1c38bab5d3f728c52fc3cc10a363d2567330145073'
